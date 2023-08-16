9e338e2090239a19239d048c58f7368fce91d386
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-08-16 09:59:23 +0530

    Created Project

---
+++ Source/EditorScripting/EditorScriptingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EditorScriptingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EDITORSCRIPTING_API AEditorScriptingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
